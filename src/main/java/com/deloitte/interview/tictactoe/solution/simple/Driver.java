9c7cf38a415d13d0005c34b422c3e80f40adc4c2
Sean M. Daffron <sdaffron@deloitte.com>
2019-06-11 13:37:52 -0400

    Updates to tic tac toe implementation for 3 by 3

--- src/main/java/com/deloitte/interview/tictactoe/solution/simple/Driver.java
+++ src/main/java/com/deloitte/interview/tictactoe/solution/simple/Driver.java
@@ -3,18 +3,16 @@ package com.deloitte.interview.tictactoe.solution.simple;
 import com.deloitte.interview.tictactoe.solution.simple.TicTacToe.PlayerMarker;
 
 public class Driver {
-
 	public static void main(String[] args) {
 		/*
 		 * Driver code for game as shown in below graphic
 		 * 
-		 *  | | | |X|
-		 *  | |O|X| |
-		 *  | |X| | |
-		 *  |X|O|O| |
+		 *  | |O|X|
+		 *  | |X| |
+		 *  |X|O|O|
 		 */
 
-		TicTacToe game = new TicTacToe(4);
+		TicTacToe game = new TicTacToe(3);
 		game.play(PlayerMarker.X, 0, 0);
 		game.play(PlayerMarker.O, 1, 0);
 		
@@ -23,7 +21,5 @@ public class Driver {
 		
 		game.play(PlayerMarker.X, 2, 2);
 		game.play(PlayerMarker.O, 2, 0);
-		
-		game.play(PlayerMarker.X, 3, 3);
 	}
 }
